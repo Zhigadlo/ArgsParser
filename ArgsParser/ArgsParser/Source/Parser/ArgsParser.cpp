ed53bebd5d71b565e03930bb3a867979dd87c47b
Vladislav Zhogol <vzhogol176@gmail.com>
2024-03-26 12:14:32 +0300

    Argument parser update

--- ArgsParser/ArgsParser/Source/Parser/ArgsParser.cpp
+++ ArgsParser/ArgsParser/Source/Parser/ArgsParser.cpp
@@ -1,14 +1,26 @@
-#include "ArgsParser.h"
+#include "ArgsParser.hpp"
+#include <iostream>
+#include "../abstractions/Arg.hpp"
 
-bool Parse(int argC, const char** argV)
+namespace parser
 {
-	
-}
-void Add(Abstractions::Arg& arg)
-{
+	bool ArgsParser::Parse(int argC, const char** argV)
+	{
+		for (int i = 0; i < argC; i += 2)
+		{
 
+		}
+	}
+	void ArgsParser::Add(abstractions::Arg& arg)
+	{
+		args.push_back(arg);
+	}
+	void ArgsParser::Show()
+	{
+		for (int i = 0; i < args.size(); i++)
+		{
+			std::cout << &args[i].GetInfo() << std::endl;
+		}
+	}
 }
-void Show()
-{
 
-}
---
+++ ArgsParser/ArgsParser/source/parser/ArgsParser.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../abstractions/Arg.hpp";
+#include <vector>
+
+namespace parser
+{
+	class ArgsParser
+	{
+	private:
+		std::vector<abstractions::Arg&> args;
+	public:
+		bool Parse(int argC, const char** argV);
+		void Add(abstractions::Arg& arg);
+		void Show();
+	};
+}

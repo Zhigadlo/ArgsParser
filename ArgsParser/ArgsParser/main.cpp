d5e7bedb33bf6cc8d2f95fdc195199c2a6a7bb62
Vladislav Zhogol <vzhogol176@gmail.com>
2024-04-15 11:29:45 +0300

    demo app fix

--- ArgsParser/ArgsParser/main.cpp
+++ ArgsParser/ArgsParser/main.cpp
@@ -4,68 +4,58 @@
 #include <validators/Validators.hpp>
 #include <iostream>
 
-results::Result ConfigureParser(parser::ArgsParser& parser);
-std::vector<args::BaseArg*> arguments;
-
-validators::PositiveIntValidator posValidator;
-validators::StringLengthValidator lenValidator(5);
-validators::IntRangeValidator rangeValidator(-5, 5);
-validators::StringLengthValidator multiStringLenValidator(8);
-validators::IPValidator ipValidator;
-
-args::HelpArg helpArg('h', "help", arguments);
-args::EmptyArg testArg('t', "test");
-args::EmptyArg multiEmptyArg('e', "m_empty", true);
-args::ValueArg<int> intArg("int_value");
-args::ValueArg<int> shortIntArg('k', &rangeValidator);
-args::ValueArg<int> positiveIntArg("positive_int", &posValidator);
-args::ValueArg<unsigned int> uintArg("uint");
-args::ValueArg<long long> longLong('l', "long_long");
-args::ValueArg<float> floatArg('f', "float_value");
-args::ValueArg<bool> boolArg('b', "bool_value");
-args::ValueArg<std::string> stringArg("string", &lenValidator);
-args::MultiValueArg<std::string> multiStringArg('s', "m_string", &multiStringLenValidator);
-args::MultiValueArg<std::string> multiIntArg('i', "m_int");
-args::ValueArg<std::string> ipArg("ip_address", &ipValidator);
+void AddArgToParser(parser::ArgsParser& parser, args::BaseArg& arg);
 
 int main(int argC, const char* argV[])
 {
+	validators::PositiveIntValidator posValidator;
+	validators::StringLengthValidator lenValidator(5);
+	validators::IntRangeValidator rangeValidator(-5, 5);
+	validators::StringLengthValidator multiStringLenValidator(8);
+	validators::IPValidator ipValidator;
+
+	//args::HelpArg helpArg('h', "help", arguments);
+	args::EmptyArg testArg('t', "test");
+	args::EmptyArg multiEmptyArg('e', "m_empty", true);
+	args::ValueArg<int> intArg("int_value");
+	args::ValueArg<int> shortIntArg('k', &rangeValidator);
+	args::ValueArg<int> positiveIntArg("positive_int", &posValidator);
+	args::ValueArg<unsigned int> uintArg("uint");
+	args::ValueArg<long long> longLong('l', "long_long");
+	args::ValueArg<float> floatArg('f', "float_value");
+	args::ValueArg<bool> boolArg('b', "bool_value");
+	args::ValueArg<std::string> stringArg("string", &lenValidator);
+	args::MultiValueArg<std::string> multiStringArg('s', "m_string", &multiStringLenValidator);
+	args::MultiValueArg<int> multiIntArg('i', "m_int");
+	args::ValueArg<std::string> ipArg("ip_address", &ipValidator);
+
 	parser::ArgsParser parser;
-	results::Result configureResult = ConfigureParser(parser);
-	if (configureResult.IsSucceded())
-	{
-		results::Result result = parser.Parse(argC, argV);
-		if (result.IsSucceded())
-			parser.Show();
-		else
-			std::cout << result.GetError() << std::endl;
-	}
+	AddArgToParser(parser, testArg);
+	AddArgToParser(parser, intArg);
+	AddArgToParser(parser, shortIntArg);
+	AddArgToParser(parser, positiveIntArg);
+	AddArgToParser(parser, uintArg);
+	AddArgToParser(parser, floatArg);
+	AddArgToParser(parser, boolArg);
+	AddArgToParser(parser, multiStringArg);
+	AddArgToParser(parser, multiEmptyArg);
+	AddArgToParser(parser, multiIntArg);
+	AddArgToParser(parser, stringArg);
+	AddArgToParser(parser, ipArg);
+	AddArgToParser(parser, longLong);
+
+	results::Result result = parser.Parse(argC, argV);
+	if (result.IsSucceded())
+		parser.Show();
 	else
-		std::cout << configureResult.GetError() << std::endl;
+		std::cout << result.GetError() << std::endl;
+
 }
-
-results::Result ConfigureParser(parser::ArgsParser& parser)
+void AddArgToParser(parser::ArgsParser& parser, args::BaseArg& arg)
 {
-	arguments.push_back(&helpArg);
-	arguments.push_back(&testArg);
-	arguments.push_back(&intArg);
-	arguments.push_back(&shortIntArg);
-	arguments.push_back(&positiveIntArg);
-	arguments.push_back(&uintArg);
-	arguments.push_back(&floatArg);
-	arguments.push_back(&boolArg);
-	arguments.push_back(&multiStringArg);
-	arguments.push_back(&multiEmptyArg);
-	arguments.push_back(&multiIntArg);
-	arguments.push_back(&stringArg);
-	arguments.push_back(&ipArg);
-	arguments.push_back(&longLong);
+	results::Result addResult = parser.Add(arg);
+	if (addResult.IsSucceded()) return;
 
-	for (int i = 0; i < arguments.size(); i++)
-	{
-		results::Result result = parser.Add(*arguments[i]);
-		if (!result.IsSucceded()) return result;
-	}
-
-	return results::Result::Success();
+	std::cout << addResult.GetError() << std::endl;
+	std::cout << arg.GetInfo() << " : Argument was not added to parser" << std::endl;
 }

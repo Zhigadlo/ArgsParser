e20e43e732634826e3bd0e10297e5b76e5908e4d
Vladislav Zhogol <74291930+Zhigadlo@users.noreply.github.com>
2024-04-20 00:33:12 +0300

    Move Catalog and ThreadPool to folderInfo main.cpp

--- ArgsParser/ArgsParser/folderInfo/main.cpp
+++ ArgsParser/ArgsParser/folderInfo/main.cpp
@@ -1,23 +1,188 @@
-#include <catalogs/Catalog.hpp>
-#include <threads/ThreadPool.hpp>
 #include <args/BaseArg.hpp>
 #include <parser/ArgsParser.hpp>
-#include <thread>
+#include <condition_variable> 
+#include <functional> 
+#include <iostream> 
+#include <mutex> 
+#include <queue> 
+#include <thread> 
+#include <filesystem>
 #include <chrono>
 
-void CatalogHandle(catalogs::Catalog& catalog, threads::ThreadPool& pool, std::chrono::milliseconds sleepMs)
+class ThreadPool
+{
+public:
+	ThreadPool(int num_threads)
+	{
+		for (int i = 0; i < num_threads; ++i)
+		{
+			threads.emplace_back([this]
+				{
+					while (true)
+					{
+						std::function<void()> task;
+						// unlocks the queue before executing the task 
+						{
+							std::unique_lock<std::mutex> lock(queueMutex);
+
+							queueCV.wait(lock, [this] { return !tasks.empty() || stop; });
+
+							if (stop && tasks.empty()) return;
+
+							task = std::move(tasks.front());
+							tasks.pop();
+						}
+
+						task();
+						{
+							std::unique_lock<std::mutex> taskCounterLock(taskCounterMutex);
+							--taskCounter;
+						}
+
+						// Notify the main thread that a task has been completed
+						taskCounterCV.notify_one();
+					}
+				});
+		}
+	}
+
+	~ThreadPool()
+	{
+		{
+			// Lock the queue to update the stop flag safely 
+			std::unique_lock<std::mutex> lock(queueMutex);
+			stop = true;
+		}
+
+		// Notify all threads 
+		queueCV.notify_all();
+
+		for (auto& thread : threads)
+			if (thread.joinable()) thread.join();
+	}
+	void WaitForTasksToFinish()
+	{
+		std::unique_lock<std::mutex> lock(taskCounterMutex);
+		taskCounterCV.wait(lock, [this] { return taskCounter == 0; });
+	}
+	void Enqueue(std::function<void()> task)
+	{
+		{
+			std::unique_lock<std::mutex> lock(queueMutex);
+
+			// Increment the task counter when a new task is enqueued
+			{
+				std::unique_lock<std::mutex> taskCounterLock(taskCounterMutex);
+				++taskCounter;
+			}
+
+			tasks.emplace(move(task));
+		}
+		queueCV.notify_one();
+	}
+private:
+	std::vector<std::thread> threads;
+	std::queue<std::function<void()>> tasks;
+	std::mutex queueMutex;
+	std::condition_variable queueCV;
+
+	// Indicate whether the thread pool should stop or not 
+	bool stop = false;
+
+	// Counter for the number of enqueued tasks
+	int taskCounter = 0;
+
+	std::mutex taskCounterMutex;
+	std::condition_variable taskCounterCV;
+};
+
+class Catalog
+{
+public:
+	Catalog(std::filesystem::path fullPath) : fullPath(std::move(fullPath)) {}
+	Catalog(std::filesystem::path fullPath, 
+						  int catalogLevel) : fullPath(std::move(fullPath)),
+										      catalogLevel(catalogLevel) {}
+
+	[[nodiscard]] const std::filesystem::path& GetFullPath() const
+	{
+		return fullPath;
+	}
+	[[nodiscard]] const std::vector<std::filesystem::path>& GetFiles() const
+	{
+		return files;
+	}
+	[[nodiscard]] std::list<Catalog>& GetCatalogs()
+	{
+		return childCatalogs;
+	}
+	void AddFile(const std::filesystem::path& fullPath)
+	{
+		files.push_back(fullPath);
+	}
+	void AddCatalog(const std::filesystem::path& fullPath)
+	{
+		Catalog newCatalog(fullPath, catalogLevel + 1);
+		childCatalogs.push_back(newCatalog);
+	}
+	void FindFiles()
+	{
+		for (auto const& dir_entry : std::filesystem::directory_iterator{ fullPath })
+		{
+			if (dir_entry.is_directory())
+				AddCatalog(dir_entry.path());
+			else
+				AddFile(dir_entry.path());
+		}
+	}
+	void ShowInfo() const
+	{
+		for (int j = 0; j < catalogLevel; j++)
+			std::cout << utils::SpaceChar << utils::SpaceChar;
+		std::cout << fullPath.filename().string() << "[" << threadIndex << "]" << std::endl;
+		if (!files.empty())
+		{
+			for (size_t i = 0; i < files.size(); i++)
+			{
+				for (int j = 0; j < catalogLevel + 1; j++)
+					std::cout << utils::SpaceChar << utils::SpaceChar;
+				std::cout << files[i].filename().string() << std::endl;
+			}
+		}
+
+		if (!childCatalogs.empty())
+		{
+			for (const Catalog& catalog : childCatalogs)
+			{
+				catalog.ShowInfo();
+			}
+		}
+	}
+	void SetThreadIndex(std::thread::id index)
+	{
+		threadIndex = index;
+	}
+private:
+	std::filesystem::path fullPath;
+	std::vector<std::filesystem::path> files;
+	std::list<Catalog> childCatalogs;
+	int catalogLevel = 0;
+	std::thread::id threadIndex;
+};
+
+void CatalogHandle(Catalog& catalog, ThreadPool& pool, std::chrono::milliseconds sleepMs)
 {
 	catalog.SetThreadIndex(std::this_thread::get_id());
 	catalog.FindFiles();
-	std::list<catalogs::Catalog>& childCatalogs = catalog.GetCatalogs();
+	std::list<Catalog>& childCatalogs = catalog.GetCatalogs();
 	if (childCatalogs.empty()) return;
-	for (catalogs::Catalog& childCatalog : childCatalogs)
+	for (Catalog& childCatalog : childCatalogs)
 	{
-		pool.Enqueue([&childCatalog, &pool, sleepMs]() 
-		{ 
-			CatalogHandle(childCatalog, pool, sleepMs);
-			std::this_thread::sleep_for(sleepMs);
-		});
+		pool.Enqueue([&childCatalog, &pool, sleepMs]()
+			{
+				CatalogHandle(childCatalog, pool, sleepMs);
+				std::this_thread::sleep_for(sleepMs);
+			});
 	}
 }
 void AddArgToParser(parser::ArgsParser& parser, args::BaseArg& arg)
@@ -59,26 +224,26 @@ int main(const int argC, const char* argV[])
 		std::cout << "Error: You need to use -t or --thread_count and define thread count" << std::endl;
 		return 0;
 	}
-	const std::filesystem::path path{ folderPathArg.GetValue()};
-	
+	const std::filesystem::path path{ folderPathArg.GetValue() };
+
 	if (!std::filesystem::exists(path))
 	{
 		std::cout << "Error: There is no such path " << path << std::endl;
 		return 0;
 	}
 
-	catalogs::Catalog root{ path };
-	threads::ThreadPool pool{ threadCountArg.GetValue() };
+	Catalog root{ path };
+	ThreadPool pool{ threadCountArg.GetValue() };
 	std::chrono::milliseconds sleepMs(0);
-	
-	if(debugSleepMsArg.IsDefined())
+
+	if (debugSleepMsArg.IsDefined())
 		sleepMs = debugSleepMsArg.GetValue();
 
 	pool.Enqueue([&root, &pool, sleepMs]()
-	{
-		CatalogHandle(root, pool, sleepMs);
-	});
+		{
+			CatalogHandle(root, pool, sleepMs);
+		});
 	pool.WaitForTasksToFinish();
-	
+
 	root.ShowInfo();
 }
